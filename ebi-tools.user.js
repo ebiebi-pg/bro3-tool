d86643b95e416708811f340d46c4697f87c76459
ebiebi-pg <kojimitu.sky@gmail.com>
2022-07-12 21:09:58 +0900

    userscript追加

---
+++ ebi-tools.user.js
@@ -0,0 +1,70 @@
+// ==UserScript==
+// @name         ebi-tool
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  同盟遠征支援ツール
+// @author       Ebi
+// @match        https://*.3gokushi.jp/*
+// @match        http://*.3gokushi.jp/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=3gokushi.jp
+// @grant        none
+// ==/UserScript==
+
+var d = document;
+
+var $a = function(xp,dc) {
+           var r = d.evaluate(xp, dc||d, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+           var a=[];
+           for(var i=0; i<r.snapshotLength; i++){
+             a.push(r.snapshotItem(i));
+           }
+           return a;
+         };
+
+var $x = function(xp,dc) {
+           return d.evaluate(xp, dc||d, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+         };
+
+var SCRIPTS = [
+               '//bro3-tool.com/bro3-tool/ebi-tool.js'
+               ];
+
+var LOADSCRIPTS = 0;
+
+(function() {
+    try {
+        if(!location.href.match(/info/)){
+            var srcElm = $x("//script[@src='" + SCRIPTS[SCRIPTS.length - 1] + "']");
+            if(srcElm != null && srcElm != undefined){
+                main();
+            }else{
+                scriptLoad();
+            }
+        }
+    }catch(e){
+        alert(e + "\n\n" + e.stack + "\n\n" + location.pathname);
+    }
+})();
+
+// ソース読み込み
+function scriptLoad(){
+    if(SCRIPTS[LOADSCRIPTS] != undefined){
+        if(LOADSCRIPTS == 0){
+            let s = document.createElement("script");
+            s.type = "text/javascript";
+            document.head.append(s);
+        }
+        try {
+            let script = document.createElement('script');
+            script.src = SCRIPTS[LOADSCRIPTS];
+            document.body.appendChild(script);
+            if(LOADSCRIPTS++ < SCRIPTS.length){
+                script.onload = function(){ scriptLoad(); };
+            }
+        }catch(e){
+            alert("Script Load Error:" + e + "\n\n" + e.stack + "\n\n" );
+        }
+    }else{
+        main();
+    }
+}
